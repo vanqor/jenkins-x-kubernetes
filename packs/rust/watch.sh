d7e39a984f9d99c835d86da3f2c90f34e8bf7e3c
Stephen Stubbs <stephen.stubbs@vanqor.com>
2020-01-05 12:38:48 +0400

    add 4 jobs

--- packs/rust/watch.sh
+++ packs/rust/watch.sh
@@ -2,8 +2,8 @@
 
 # watch the java files and continously deploy the service
 export UUID=$(uuidgen)
-cargo install --path . --root ./apps --force
+cargo install --path . --root ./apps --force -j 4
 skaffold run -p dev
-reflex -r "\.rs$" -- bash -c 'export UUID=$(uuidgen) && cargo install --path . --root ./apps --force && skaffold run -p dev'
+reflex -r "\.rs$" -- bash -c 'export UUID=$(uuidgen) && cargo install --path . --root ./apps --force -j 4 && skaffold run -p dev'
 skaffold delete -p dev
 docker image prune -af
